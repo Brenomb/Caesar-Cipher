a621987fa2694cfb4f2e9dc428a53593ba67ce9a
Brenomb <brenomaistrobr@gmail.com>
2024-02-14 17:38:21 -0300

    Caesar Cipher 1.1

--- main.py
+++ main.py
@@ -4,16 +4,29 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 def caesar(text, shift, direction):
     cipher_Text = ''
+    shift = shift % 26
     if direction == 'decode':
         shift *= -1
-    for letter in text:
-        position = alphabet.index(letter)
-        new_Position = position + shift
-        cipher_Text += alphabet[new_Position]
+    for char in text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_Position = position + shift
+            cipher_Text += alphabet[new_Position]
+        else:
+            cipher_Text += char
     print(f'{direction}d text: {cipher_Text}')
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
-text = input("Type your message: \n").lower()
-shift = int(input("Type the shift number: \n"))
+direction = input("Type 'encode' to encrypt or 'decode' to decrypt and 'exit' to stop \n ")
 
-caesar(text, shift, direction)
+while direction != 'exit':
+    text = input("Type your message: \n").lower()
+    shift = int(input("Type the shift number: \n"))
+    match direction:
+        case 'encode':
+            caesar(text, shift, direction)
+        case 'decode':
+            caesar(text, shift, direction)
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
+
+
+
